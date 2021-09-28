e643490be2177438c051bd0548e4f6bff3f535e5
Jobert Manosca <joberttm@hotmail.com>
2021-09-28 17:24:35 -0400

    Crypto details added

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -15,9 +15,12 @@ export const cryptoApi = createApi({
   endpoints: (builder) => ({
     getCryptos: builder.query({
       query: (count) => createRequest(`/coins?limit=${count}`)
+    }),
+    getCryptoDetails: builder.query({
+      query: (coinId) => createRequest(`/coin/${coinId}`)
     })
   })
 });
 
 // Redux toolkit automatically creates hooks for your endpoints
-export const { useGetCryptosQuery } = cryptoApi;
+export const { useGetCryptosQuery, useGetCryptoDetailsQuery } = cryptoApi;
